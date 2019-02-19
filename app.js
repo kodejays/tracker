2f132396a9520565bfae3814b46979af94256d56
Jeevan Sai <kjeevansai@gmail.com>
2019-02-20 00:49:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const { parse } = require('querystring');
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST') {
+        collectRequestData(req, result => {
+            console.log(result);
+            res.end(`
+            <!doctype html>
+            <html>
+            <body>
+                <form action="/" method="post">
+                    <span> Hi ${result.fname} </span><br />
+                    <input type="text" name="fname" /><br />
+                    <button>Save</button>
+                </form>
+            </body>
+            </html>
+        `);
+        });
+    } 
+    else {
+        res.end(`
+            <!doctype html>
+            <html>
+            <body>
+                <form action="/" method="post">
+                    <input type="text" name="fname" /><br />
+                    <button>Save</button>
+                </form>
+            </body>
+            </html>
+        `);
+    }
+});
+server.listen(3000);
+
+function collectRequestData(request, callback) {
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if(request.headers['content-type'] === FORM_URLENCODED) {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(parse(body));
+        });
+    }
+    else {
+        callback(null);
+    }
+}
